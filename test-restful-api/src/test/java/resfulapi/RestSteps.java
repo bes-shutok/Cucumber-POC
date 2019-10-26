a6e3ccf89e03e6708229a198a54fd6d9b33b6dd0
Andrey Dmitriev <davsight@yandex.ru>
2019-10-26 10:08:10 +0300

    Successful setup of the requests

--- test-restful-api/src/test/java/resfulapi/RestSteps.java
+++ test-restful-api/src/test/java/resfulapi/RestSteps.java
@@ -1,9 +1,12 @@
 package resfulapi;
 
+import io.cucumber.core.logging.Logger;
+import io.cucumber.core.logging.LoggerFactory;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.glassfish.jersey.client.*;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.testng.Assert;
 
@@ -11,19 +14,27 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import java.net.HttpURLConnection;
 
 public class RestSteps {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     @Given("The Lambda Integration server is on and FHIRE API responds on \\/metadata request")
-    public void theLambdaIntegrationServerIsOnAndFHIREAPIIsRespondsOnMetadataRequest() throws Throwable {
+    public void theLambdaIntegrationServerIsOnAndFHIREAPIRespondsOnMetadataRequest() throws Throwable {
         try {
-            Client client = ClientBuilder.newClient( new ClientConfig().register( LoggingFeature.class ) );
+            HttpAuthenticationFeature feature =
+                    HttpAuthenticationFeature.basic("vcp_dev", "y6s3fmZR4j");
+            final Client client = ClientBuilder.newClient(
+                    new ClientConfig().register( LoggingFeature.class ).register(feature) );
+
             WebTarget webTarget = client.target("http://aws-integration-dev.datafusion.idexx.com/fhir/r4" +
-                    "/ConceptMap/gender-map/$translate?target=http://snomed.info/sct" + "/m");
-            Invocation.Builder invocationBuilder =  webTarget.request();
+                    "/metadata");
+            logger.info("Trying to invoke the " + webTarget.toString());
+            Invocation.Builder invocationBuilder =  webTarget.request("application/json+fhir");
             Response response = invocationBuilder.get();
-            System.out.println(response.getStatus());
+            Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK,
+                    "Did not receive OK, the response: " + response.getStatus());
             System.out.println(response.readEntity(String.class));
 
         } catch (RuntimeException r) {
@@ -32,18 +43,55 @@ public class RestSteps {
             System.out.println("Exception caught");
             e.printStackTrace();
         }
-        //Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK,  "Did not receive OK response: ");
     }
 
     @When("We are requesting FHIRE response for code m")
     public void weAreRequestingFHIREResponseForCodeM() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        try {
+            HttpAuthenticationFeature feature =
+                    HttpAuthenticationFeature.basic("vcp_dev", "y6s3fmZR4j");
+            final Client client = ClientBuilder.newClient(
+                    new ClientConfig().register( LoggingFeature.class ).register(feature) );
+
+            WebTarget webTarget = client.target("http://aws-integration-dev.datafusion.idexx.com/fhir/r4" +
+                    "/ConceptMap/gender-map/$translate?target=http://snomed.info/sct" + "&code=m");
+            logger.info("Trying to invoke the " + webTarget.toString());
+            Invocation.Builder invocationBuilder =  webTarget.request("application/json+fhir");
+            Response response = invocationBuilder.get();
+            Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK,
+                    "Did not receive OK, the response: " + response.getStatus());
+            System.out.println(response.readEntity(String.class));
+
+        } catch (RuntimeException r) {
+            throw r;
+        } catch (Exception e) {
+            System.out.println("Exception caught");
+            e.printStackTrace();
+        }
     }
 
     @Then("the response should be JSON:")
     public void theResponseShouldBeJSON(String docString) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        try {
+            HttpAuthenticationFeature feature =
+                    HttpAuthenticationFeature.basic("vcp_dev", "y6s3fmZR4j");
+            final Client client = ClientBuilder.newClient(
+                    new ClientConfig().register( LoggingFeature.class ).register(feature) );
+
+            WebTarget webTarget = client.target("http://aws-integration-dev.datafusion.idexx.com/fhir/r4" +
+                    "/ConceptMap/gender-map/$translate?target=http://snomed.info/sct" + "&code=m");
+            logger.info("Trying to invoke the " + webTarget.toString());
+            Invocation.Builder invocationBuilder =  webTarget.request("application/json+fhir");
+            Response response = invocationBuilder.get();
+            Assert.assertEquals(response.getStatus(), HttpURLConnection.HTTP_OK,
+                    "Did not receive OK, the response: " + response.getStatus());
+            System.out.println(response.readEntity(String.class));
+
+        } catch (RuntimeException r) {
+            throw r;
+        } catch (Exception e) {
+            System.out.println("Exception caught");
+            e.printStackTrace();
+        }
     }
 }
